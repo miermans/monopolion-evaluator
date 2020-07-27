990ba8e523871f1f2da23d06e6c39c368a975403
Mathijs Miermans <m.miermans@gmail.com>
2020-07-26 21:00:59 -0700

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='monopolion-evaluator',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     description='Neural net to predict winning probability in Monopoly',
     long_description='%s\n%s' % (
