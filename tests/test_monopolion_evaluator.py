b8713befcbe984f07452f2a9b5435f1b0918ccf1
Mathijs Miermans <m.miermans@gmail.com>
2020-07-28 08:20:19 -0700

    Increase cli test coverage

--- tests/test_monopolion_evaluator.py
+++ tests/test_monopolion_evaluator.py
@@ -4,3 +4,10 @@ from monopolion_evaluator.cli import main
 
 def test_main():
     main(['--training_data=tests/fixtures/toy_data_2player.gz', '--epochs=1'])
+
+
+def test_main_validation():
+    main([
+        '--training_data=tests/fixtures/toy_data_2player.gz',
+        '--validation_data=tests/fixtures/toy_data_2player.gz',
+        '--epochs=1'])
