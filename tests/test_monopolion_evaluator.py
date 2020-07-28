4e2b588fea6f40184d14c50dff9d1f8009efe3c0
Mathijs Miermans <m.miermans@gmail.com>
2020-07-28 08:36:44 -0700

    Fixed test_main

--- tests/test_monopolion_evaluator.py
+++ tests/test_monopolion_evaluator.py
@@ -3,7 +3,7 @@ from monopolion_evaluator.cli import main
 
 
 def test_main():
-    main(['--training_data=tests/fixtures/toy_data_2player.gz', '--epochs=1'])
+    main(['--training=tests/fixtures/toy_data_2player.gz', '--epochs=1'])
 
 
 def test_main_validation():
